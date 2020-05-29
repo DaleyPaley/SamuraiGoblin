4358f60f4871afdf0f6edcf354d227321cb66c59
Dale Thomas <daley.paley@gmail.com>
2020-05-29 23:15:57 +0900

    adding ppm tutorial

---
+++ Tutorials/PPM/ppm.cpp
@@ -0,0 +1,2 @@
+python data/upload_video.py --file="/Users/dale/Tmp/AB_man_and_snake.avi" --title="The Man and the Snake by Ambrose Bierce - Audiobook" --description="The Man and the Snake by Ambrose Bierce - Audiobook - Don't forget to like and subscribe" --keywords="audiobook,classics,Bierce,short stories,public domain,horror" --category="24" --privacyStatus="public"
+/Users/dale/Tmp/AB_man_and_snake.png
